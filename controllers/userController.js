785fe1eb86bab98a828edbb9c7e32b7c7953faa6
Roshan <roshan.jha@hih7.in>
2023-06-06 12:36:40 +0530

    Register User Endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import User from "../model/userModel.js";
 
 // @desc Auth user/set token
 // route POST /api/users/auth
@@ -13,7 +14,33 @@ const authUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register User" });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+  //   res.status(200).json({ message: "Register User" });
 });
 
 // @desc Logout User
