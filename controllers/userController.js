e860a25ae95c85c625214fcd99f95747a8af5d72
Roshan <roshan.jha@hih7.in>
2023-06-06 11:19:26 +0530

    User Routes & Controller Setup  Custom Error Middleware

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+// @desc Auth user/set token
+// route POST /api/users/auth
+// @access Public
+
+import asyncHandler from "express-async-handler";
+
+const authUser = asyncHandler(async (req, res) => {
+  res.status(401);
+  throw new Error("Something went wrong");
+  res.status(200).json({ message: "Auth User" });
+});
+
+export { authUser };
