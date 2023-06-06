0fe33c84ed81cf81f5e04812ad49fc5556637926
Roshan <roshan.jha@hih7.in>
2023-06-06 11:30:45 +0530

    User Controller Functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,49 @@
+import asyncHandler from "express-async-handler";
+
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 
-import asyncHandler from "express-async-handler";
-
 const authUser = asyncHandler(async (req, res) => {
-  res.status(401);
-  throw new Error("Something went wrong");
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+// @desc Register a new User
+// route POST /api/users
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc Logout User
+// route POST /api/users/logout
+// @access Public
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "logout User" });
+});
+
+// @desc get User Profile
+// route POST /api/users/profile
+// @access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+// @desc update User Profile
+// route POST /api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile Updated" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
